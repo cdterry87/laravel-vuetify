426b5952699c5d83afda1fb643bb9e551502f404
Chase Terry <chase.terry87@gmail.com>
2020-07-14 22:17:55 -0500

    Fixed an issue with router.

--- resources/js/router.js
+++ resources/js/router.js
@@ -5,19 +5,24 @@ import Home from './views/Home'
 import Account from './views/Account'
 import NotFound from './views/NotFound'
 
-export default [
-  {
-    path: '/home',
-    name: 'home',
-    component: Home,
-  },
-  {
-    path: '/account',
-    name: 'account',
-    component: Account
-  },
-  {
-    path: '*',
-    component: NotFound
-  }
-]
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/home',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/account',
+      name: 'account',
+      component: Account
+    },
+    {
+      path: '*',
+      component: NotFound
+    }
+  ]
+})
